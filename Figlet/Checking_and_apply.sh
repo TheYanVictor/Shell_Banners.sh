5c4533828657ec79b5d0cd78863be0b54c3065e1
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-17 17:44:46 -0300

    Create Checking_and_apply.sh

---
+++ Figlet/Checking_and_apply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if figlet is installed
+if ! command -v figlet &> /dev/null; then
+    echo "Figlet not found. Installing..."
+    sudo yum install -y epel-release
+    sudo yum install -y figlet
+    echo "Figlet installed successfully."
+else
+    echo "Figlet is already installed."
+fi
+
+# Print it
+figlet  Yan Victor -f slant
